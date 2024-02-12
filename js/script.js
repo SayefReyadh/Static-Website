294e4e39a2b07f41264df2c3ced144d35b852839
SayefReyadh <sayef.reyadh.aust@gmail.com>
2024-02-12 15:35:29 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
 console.log("Welcome to Sayef Reyadh's website!");
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    alert("Welcome to Sayef Reyadh's website!");
-});
